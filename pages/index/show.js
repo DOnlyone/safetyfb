e20d2f51b7238fc3caff82fbb0e15db42040c369
DOnlyone <798451684@qq.com>
2019-12-01 22:27:19 +0800

    initRepo

---
+++ pages/index/show.js
@@ -0,0 +1,34 @@
+Page({
+  data: {
+    active: 0,
+    tabs: {
+      currentIndex: 0,
+      list: [{
+        text: '安全',
+        type: '1'
+      }, {
+        text: '有情况反馈',
+        type: '2'
+      }]
+    }
+  },
+  onChange(event) {
+    // event.detail 的值为当前选中项的索引
+    this.setData({ active: event.detail });
+  },
+  changeTab: function (e) {
+    console.log(e);
+  },
+  onChange(event) {
+    wx.showToast({
+      icon: 'none',
+      title: `切换至第${event.detail}项`
+    });
+  },
+  onChange(event) {
+    wx.showToast({
+      icon: 'none',
+      title: `切换至第${event.detail}项`
+    });
+  }
+});
