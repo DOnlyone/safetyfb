e1ed354adb27830d468f758a30835bd97d249189
lihaibo <lihaibo@sunboxsoft.com>
2019-12-02 17:27:00 +0800

    安全上报页面编辑

---
+++ pages/work/show.js
@@ -0,0 +1,89 @@
+// pages/work/show.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    selectData:[{value:1,text:"city"},
+      { value: 1, text: "city" }],
+      city:"city",
+    cities:"cities",
+    option1: [
+      { text: '全部商品', value: 0 },
+      { text: '新款商品', value: 1 },
+      { text: '活动商品', value: 2 }
+    ]
+  },
+  cityChanged({ detail }){
+    console.log(detail);
+  },
+  onClickLeft() {
+    wx.showToast({ title: '点击返回', icon: 'none' });
+  },
+  onClickRight() {
+    wx.showToast({ title: '点击按钮', icon: 'none' });
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({
+      option1: [
+        { text: '全部商品1', value: 0 },
+        { text: '新款商品1', value: 1 },
+        { text: '活动商品1', value: 2 }
+      ]
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
