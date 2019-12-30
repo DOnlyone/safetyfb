c745d1b1e0f07846f00027f2886d0c74a3ea83c0
DOnlyone <798451684@qq.com>
2019-12-30 22:19:36 +0800

    完善登陆功能

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,146 +1,78 @@
 
 // pages/login/login.js
-
 const app = getApp()
 
 Page({
 
   /**
-
    * 页面的初始数据
-
    */
-
   data: {
-
     disabled: false,
-
     no: '',
-
     pwd: '',
-
     noinput: false,
-
     pwdinput: false,
-
   },
 
   noinput: function (e) {
-
     this.setData({ no: e.detail.value });
-
     this.setData({ noinput: true });
-
     if (this.data.noinput == true && this.data.pwdinput == true) {
-
       this.setData({ disabled: false });
-
     }
-
-
-
   },
 
   pwdinput: function (e) {
-
     this.setData({ pwd: e.detail.value });
-
     this.setData({ pwdinput: true });
-
     if (this.data.noinput == true && this.data.pwdinput == true) {
-
       this.setData({ disabled: false });
-
     }
-
   },
 
   formSubmit: function (e) {
     console.log("用户提交表单");
     wx.showLoading({
-
       title: '登录中...',
-
     })
-
     console.log(e);
-
     this.setData({ disabled: true });
 
     wx.request({
-
-      url: 'http://localhost:8080/wx_login',
-
+      url: 'http://localhost:8080/api/wx_login',
       //url: app.globalData.url.login, //仅为示例，并非真实的接口地址
-
       data: {
-
-        no: e.detail.value.no,
-
-        pwd: e.detail.value.pwd
-
+        userName: e.detail.value.no,
+        password: e.detail.value.pwd
       },
 
       header: {
-
         'content-type': 'application/json' // 默认值
-
       },
 
       success: function (res) {
-
         console.log(res);
-
+        wx.hideLoading();
         if (res.statusCode == 200) {
-
-          if (res.data.error == true) {
-
+          if (res.data.success == false) {
             wx.showToast({
-
-              title: res.data.msg,
-
+              title: '用户名密码错误，请确认后重新登陆',
               icon: 'none',
-
               duration: 2000
-
             })
-
           } else {
-
             wx.setStorageSync('student', res.data.data);
-
-            wx.showToast({
-
-              title: res.data.msg,
-
-              icon: 'success',
-
-              duration: 2000
-
-            })
-
             setTimeout(function () {
-
-              wx.switchTab({
-
-                url: 'http://localhost:8080/wx_login',
-
-              })
-
+              wx.switchTab({ url: '/pages/index/show'})
             }, 2000)
-
           }
-
         } else {
-
+          wx.hideLoading();
           wx.showToast({
-
             title: '服务器出现错误',
-
             icon: 'none',
-
             duration: 2000
-
           })
 
         }
